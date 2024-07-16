b5f59f2d0d73f8d8d1394e70fa914a9daf25c8cc
Alex Plong <alexplong@gmail.com>
2024-07-16 14:28:13 -0400

    Update: refactor out validation method

--- todo.rb
+++ todo.rb
@@ -31,15 +31,25 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  return "List name must be between 1 and 100 characters." if !(1..100).cover? name.size
+  return "List name must be unique." if session[:lists].any? { |list| list[:name] == name }
+
+  nil
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if (1..100).cover? list_name.size
+  error = error_for_list_name(list_name)
+
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
     session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created."
-    redirect "/lists"
-  else
-    session[:error] = "List name must be between 1 and 100 characters"
-    erb :new_list, layout: :layout
+    redirect "/lists" 
   end
 end
