fa040172a96fad01e08ff675f1a0e3a16a7e3d4d
Alex Plong <alexplong@gmail.com>
2024-07-24 15:04:49 -0400

    Update: add async request for delete function

--- todo.rb
+++ todo.rb
@@ -139,8 +139,14 @@ end
 post '/lists/:id/destroy' do
   id = params[:id].to_i
   session[:lists].delete_at(id)
-  session[:success] = 'The list has been deleted.'
-  redirect '/lists'
+
+  # check request header
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    "/lists"
+  else
+    session[:success] = 'The list has been deleted.'
+    redirect '/lists'
+  end
 end
 
 # Add a new todo to the list
@@ -168,9 +174,15 @@ post '/lists/:list_id/todos/:id/destroy' do
 
   todo_id = params[:id].to_i
   @list[:todos].delete_at(todo_id)
-  session[:success] = "The todo has been deleted"
 
-  redirect "/lists/#{@list_id}"
+  # check request header
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    # ajax
+    status 204
+  else
+    session[:success] = "The todo has been deleted"
+    redirect "/lists/#{@list_id}"
+  end
 end
 
 # Update the status of a todo
